c041e7bf23271fb16b02060c18154a5148204d13
Adams <adams6444@gmail.com>
2023-02-16 16:08:02 +0100

    Adding small change

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/zad1/dodajlosowe/{liczba}', [WelcomeController::class, 'task3'])->name('task3');
 
-
 Route::get('/zad1/lista/{fraza}', [WelcomeController::class, 'task4'])->name('task4');
 
 Route::get('/zad6', [WelcomeController::class, 'task5'])->name('task5');
 
-Route::get('/convert/array', [WelcomeController::class, 'task6'])->name('convertArray');
+Route::get('/convert/array', [WelcomeController::class, 'task6'])->name('task6');
